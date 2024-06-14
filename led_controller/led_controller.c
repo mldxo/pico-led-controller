7fc330de623c0a60b23f2c82023d3d93aa96aa10
mldxo <miloszmaculewicz@gmail.com>
2024-06-14 13:27:06 +0200

    Add interrupts handling

--- led_controller/led_controller.c
+++ led_controller/led_controller.c
@@ -24,34 +24,62 @@
 
 #define WS2812_PIN 2
 #define LIGHT_TOGGLE_PIN 15
+#define STOP_BUTTON_PIN 16
 #define IS_RGBW false
+
+#define SPEED_FACTOR 1
+#define DENSITY_FACTOR 1
+
+volatile bool stop_flag = false;
 
 void connect_to_wifi()
 {
     printf("Connecting to Wi-Fi...\n");
     while (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 10000) != 0)
         printf("Attempting to connect...\n");
-    printf("Connected!\n");
+    printf("Connected to Wi-Fi\n");
 }
 
-void button_irq_handler(uint gpio, uint32_t events)
+void light_toggle_irq_handler(uint gpio, uint32_t events)
 {
     toggle_light_state(NUM_PIXELS);
+    gpio_set_irq_enabled(LIGHT_TOGGLE_PIN, GPIO_IRQ_EDGE_FALL, false);
 }
 
-bool init()
+void stop_button_irq_handler(uint gpio, uint32_t events)
 {
-    stdio_init_all();
+    stop_flag = true;
+}
+
+enum init_result_t
+{
+    INIT_SUCCESS,
+    STDIO_INIT_FAILURE,
+    WIFI_INIT_FAILURE
+};
+
+enum init_result_t init()
+{
+    onboard_led_blink(250, 50);
+    if (!stdio_init_all())
+    {
+        printf("Stdio init failed");
+        return STDIO_INIT_FAILURE;
+    }
     gpio_init(LIGHT_TOGGLE_PIN);
     gpio_set_dir(LIGHT_TOGGLE_PIN, GPIO_IN);
     gpio_pull_up(LIGHT_TOGGLE_PIN);
+    gpio_set_irq_enabled_with_callback(LIGHT_TOGGLE_PIN, GPIO_IRQ_EDGE_FALL, false, &light_toggle_irq_handler);
 
-    gpio_set_irq_enabled_with_callback(LIGHT_TOGGLE_PIN, GPIO_IRQ_EDGE_FALL, true, &button_irq_handler);
+    gpio_init(STOP_BUTTON_PIN);
+    gpio_set_dir(STOP_BUTTON_PIN, GPIO_IN);
+    gpio_pull_up(STOP_BUTTON_PIN);
+    gpio_set_irq_enabled_with_callback(STOP_BUTTON_PIN, GPIO_IRQ_EDGE_FALL, false, &stop_button_irq_handler);
 
     if (cyw43_arch_init())
     {
         printf("Wi-Fi init failed");
-        return false;
+        return WIFI_INIT_FAILURE;
     }
 
     cyw43_arch_enable_sta_mode();
@@ -62,27 +90,39 @@ bool init()
     
     onboard_led_blink(250, 50);
     onboard_led_blink(250, 50);
-    return true;
+    return INIT_SUCCESS;
 }
 
-void apply_rainbow_effect(uint16_t* base_hue, uint8_t* speed_factor, uint8_t* density_factor, uint8_t* brightness)
+void apply_rainbow_effect(uint* base_hue)
 {
-    for (uint8_t i = 0; i < NUM_PIXELS; ++i)
+    for (uint16_t i = 0; i < NUM_PIXELS; ++i)
     {
-        uint16_t hue = (*base_hue + i * 360 / NUM_PIXELS * *density_factor) % 360;
-        uint32_t color = hsv_to_rgb(hue * *brightness / 100, 255 * *brightness / 100, 255* *brightness / 100);
+        uint16_t hue = (*base_hue + i * 360 / NUM_PIXELS * DENSITY_FACTOR) % 360;
+        uint32_t color = hue_to_rgb(hue);
         put_pixel(color);
         hue++;
     }
-    *speed_factor = rand() % 3 + 5; // 5, 6, 7
-    *base_hue += *speed_factor;
-    *base_hue %= 360;
+    *base_hue += SPEED_FACTOR;
+    if (*base_hue >= 360)
+    {
+        printf("Resetting base hue\n");
+        *base_hue -= 360;
+    }
+    else if (*base_hue < 0)
+    {   
+        printf("Resetting base hue\n");
+        *base_hue += 360;
+    }
 }
 
 int main()
 {
-    if (!init())
-        return -1;
+    enum init_result_t init_result = init();
+    if (init_result != INIT_SUCCESS)
+    {
+        return init_result;
+    }
+
     printf("WS2812 LED Control, using pin %d\n", WS2812_PIN);
 
     PIO pio = pio0;
@@ -90,21 +130,28 @@ int main()
     uint offset = pio_add_program(pio, &ws2812_program);
     ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
 
+    set_all_blue(NUM_PIXELS, 255);
+
     puts("Beginning main loop");
-    uint16_t base_hue = 0;
-    uint8_t speed_factor = 4;
-    uint8_t density_factor = 3;
-    uint8_t brightness = 100;
+    // uint* base_hue = (uint*)malloc(sizeof(uint));
     while (true)
     {
+        if (stop_flag)
+        {
+            break;
+        }
         if (light_state)
-            apply_rainbow_effect(&base_hue, &speed_factor, &density_factor, &brightness);
+        {
+            // apply_rainbow_effect(base_hue);
+            set_all_blue(NUM_PIXELS, 255);
+        }
         if (cyw43_tcpip_link_status(&cyw43_state, CYW43_ITF_STA) != CYW43_LINK_UP)
         {
             turn_off_all(NUM_PIXELS);
             connect_to_wifi();
         }
-        sleep_ms(70);
+        sleep_ms(100);
     }
+    // free(base_hue);
     cyw43_arch_deinit();
 }
